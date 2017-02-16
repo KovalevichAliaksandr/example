d918aa97f7cb47393098d6eba53c2821b9c67fe8
KovalevichAliaksandr <servistest@gmail.com>
2017-02-16 23:47:17 +0300

    add webtest. Is worl

---
+++ webtest/src/main/java/com/departments/controller/TestServletController.java
@@ -0,0 +1,19 @@
+package com.departments.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by alex on 9.2.17.
+ */
+public class TestServletController extends HttpServlet {
+    @Override
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter printWriter=response.getWriter();
+        printWriter.println( "Servlet write");
+    }
+}
